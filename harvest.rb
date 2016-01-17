d329365c2b66e09903bcc279965fd0d56fc735fb
Atul Varma <varmaa@gmail.com>
2016-01-17 16:23:30 -0500

    factor out lib/twitter-harvester.rb.

--- harvest.rb
+++ harvest.rb
@@ -1,31 +1,10 @@
-require 'twitter'
+require_relative 'lib/twitter-harvester'
 
-require_relative 'lib/util'
-
-TWEET_COUNT = 20
-
-client = Twitter::REST::Client.new do |config|
+harvester = TwitterHarvester.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
   config.access_token = ENV['TWITTER_ACCESS_TOKEN']
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
-def harvest_tweet(client, tweet)
-  tweet.uris.each do |uri|
-    if not Util.quoted_tweet_url?(uri.expanded_url)
-      print "#{uri.expanded_url}\n"
-    end
-  end
-  if tweet.quoted_status?
-    harvest_tweet(client, tweet.quoted_status)
-  end
-end
-
-def harvest_urls(client)
-  client.home_timeline({count: TWEET_COUNT}).each do |tweet|
-    harvest_tweet(client, tweet)
-  end
-end
-
-harvest_urls client
+harvester.harvest_home_timeline 20
---
+++ lib/twitter-harvester.rb
@@ -0,0 +1,22 @@
+require 'twitter'
+
+require_relative './util'
+
+class TwitterHarvester < Twitter::REST::Client
+  def harvest_tweet(tweet)
+    tweet.uris.each do |uri|
+      if not Util.quoted_tweet_url?(uri.expanded_url)
+        print "#{uri.expanded_url}\n"
+      end
+    end
+    if tweet.quoted_status?
+      harvest_tweet(tweet.quoted_status)
+    end
+  end
+
+  def harvest_home_timeline(count)
+    home_timeline({count: count}).each do |tweet|
+      harvest_tweet(tweet)
+    end
+  end
+end
