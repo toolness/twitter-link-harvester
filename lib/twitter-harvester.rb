b2d99d2c246d3ecd91a0b7be7f58f84b8421252d
Atul Varma <varmaa@gmail.com>
2016-01-17 17:23:03 -0500

    Add Twitter::Tweet#harvest.

--- lib/twitter-harvester.rb
+++ lib/twitter-harvester.rb
@@ -1,20 +1,18 @@
 require 'twitter'
 
-class TwitterHarvester < Twitter::REST::Client
+class Twitter::Tweet
   require_relative './util'
 
-  def harvest_tweet(tweet)
-    uris = tweet.uris.select do |uri|
+  def harvest
+    links = uris.select do |uri|
       !Util.quoted_tweet_url? uri.expanded_url
     end
-    if tweet.quoted_status?
-      uris.concat harvest_tweet(tweet.quoted_status)
-    end
-    return uris
+    quoted_status? ? links.concat(quoted_status.harvest) : links
   end
+end
 
+class TwitterHarvester < Twitter::REST::Client
   def harvest_home_timeline(count)
-    home_timeline({count: count}).map {|tweet| harvest_tweet(tweet)}
-      .flatten
+    home_timeline({count: count}).map {|tweet| tweet.harvest}.flatten
   end
 end
