d0a3eb5dadea49ca2b3cb92e4e0e8e9e19147f2c
Atul Varma <varmaa@gmail.com>
2016-01-19 02:30:18 -0500

    add logging, optparse to harvest.rb.

--- harvest.rb
+++ harvest.rb
@@ -1,16 +1,49 @@
+require 'optparse'
+
 require_relative 'lib/twitter_harvester'
 require_relative 'lib/shared_link'
 require_relative 'lib/markdown_email'
 require_relative 'env'
 
-harvester = Env.create_harvester()
+module Harvest
+  def self.run!(count: 20, silent: false)
+    harvester = Env.create_harvester()
 
-links = harvester.harvest_home_timeline(20).map {|uri| SharedLink.new uri}
+    print "Fetching #{count} timeline entries...\n" unless silent
 
-email = MarkdownEmail.new links
+    links = harvester.harvest_home_timeline(count)
+    links = links.each_with_index.map do |uri, i|
+      print "Fetching link #{i} of #{links.length} " unless silent
+      print "@ #{uri.host}...\n" unless silent
+      SharedLink.new uri
+    end
 
-Env.configure_pony().mail(
-  :body => email.render_to_markdown,
-  :html_body => email.render_to_html,
-  :subject => 'Harvested Twitter Links'
-)
+    print "Sending email...\n" unless silent
+
+    email = MarkdownEmail.new links
+
+    Env.configure_pony().mail(
+      :body => email.render_to_markdown,
+      :html_body => email.render_to_html,
+      :subject => 'Harvested Twitter Links'
+    )
+  end
+
+  def self.run_cmdline!
+    options = {}
+
+    OptionParser.new do |opts|
+      opts.on('-s', '--[no-]silent', "Run silently") do |silent|
+        options[:silent] = silent
+      end
+
+      opts.on('--count N', Integer, "Harvest latest N tweets") do |n|
+        options[:count] = n
+      end
+    end.parse!
+
+    self.run!(options)
+  end
+end
+
+Harvest.run_cmdline! if __FILE__==$0
